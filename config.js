4ef6dabdda425c5d357351b231c8a68cccf43081
unknown <amartyabiswas001@gmail.com>
2018-03-15 22:19:49 +0530

    make it better

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    FB_PAGE_TOKEN: '',
+    FB_VERIFY_TOKEN: '',
+    API_AI_CLIENT_ACCESS_TOKEN: 'ab1e4dfcc44e4c2cba5cd56af3e29eac',
+    FB_APP_SECRET: '',
+    SERVER_URL: "https://smartchats.herokuapp.com/",
+};
