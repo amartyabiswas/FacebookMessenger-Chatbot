89cb117584eb1b85ad8c1fe8cb8208d07618ff4b
unknown <amartyabiswas001@gmail.com>
2018-03-15 23:39:14 +0530

    saving the days

--- app.js
+++ app.js
@@ -204,9 +204,7 @@ function handleApiAiAction(sender, action, responseText, contexts, parameters) {
             ];
             sendQuickReply(sender, responseText, replies);
             break;
-		default:
-			//unhandled action, just send back the text
-			sendTextMessage(sender, responseText);
+		
 	}
 }
 
