adee9bbea1080d7789f7602f552a918cf089ecbe
unknown <amartyabiswas001@gmail.com>
2018-03-15 22:23:54 +0530

    make it best

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    FB_PAGE_TOKEN: '',
-    FB_VERIFY_TOKEN: '',
+    FB_PAGE_TOKEN: 'EAADkkSb7mRIBADZCrWPpoerydGJurQb8fPEmIF7L1lYVZCpZAzoW6N3RQCqZBJ8Wruqw3EHuIzLQFoxO03MICYVbpNWougWxYbZBjyDME1uw7b7uX03OHQlXWN8E9ZC4kptmZAax7CdewgVovZCoWX2yaXC523vwACxDBtyaWRz5gAhDvaeqekgb',
+    FB_VERIFY_TOKEN: 'longnightsarecoming',
     API_AI_CLIENT_ACCESS_TOKEN: 'ab1e4dfcc44e4c2cba5cd56af3e29eac',
-    FB_APP_SECRET: '',
+    FB_APP_SECRET: '9ff3eb74289fd82179dfc8ae9284c7b1',
     SERVER_URL: "https://smartchats.herokuapp.com/",
 };
