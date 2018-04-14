2982d65affa17ab8ee0c18ff7a0af0f5b88a6590
Amartya Biswas <31181068+amartyabiswas@users.noreply.github.com>
2018-04-14 23:01:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
 
-    FB_PAGE_TOKEN: 'EAADkkSb7mRIBANuxrZACr4RiNehJyZBPtOEHWSkAOKYk6VR7e7sufcv0kZCYQVBawubwp0tIW7Cvd0A8tLp8Q3hAiR2WTzhCc2FaiZCGOYA1CWfhDEQtw6l0YDOk6hQ2KnuH0uo8OnKpp6QNjjJlbbKbLZCyFSd4klRNMPlB7rZBI3qJlnjDZBs',
-    FB_VERIFY_TOKEN: 'longnightsarecoming',
-    API_AI_CLIENT_ACCESS_TOKEN: '7825d4692a284bae92d57cbdc73c08c6',
-    FB_APP_SECRET: '9ff3eb74289fd82179dfc8ae9284c7b1',
-    SERVER_URL: "https://facebook-messenger-chatbots.herokuapp.com/",
-    SENDGRID_API_KEY: "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",
-    WEATHER_API_KEY: "a188cc4a27a0b163b7953e4b11fe6c77"
+    FB_PAGE_TOKEN: '',
+    FB_VERIFY_TOKEN: '',
+    API_AI_CLIENT_ACCESS_TOKEN: '',
+    FB_APP_SECRET: '',
+    SERVER_URL: "",
+    SENDGRID_API_KEY: "",
+    WEATHER_API_KEY: ""
 };
